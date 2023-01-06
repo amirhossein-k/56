d3ecf76a1f268dcd2a8ead0c72f70db8cfd72884
Amir Hosein karimi <amirhossein.k787878@gmail.com>
2023-01-06 18:13:06 +0000

    Delete 12 files

--- src/Redux/Store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import ItemReducer from "./Slice/ItemSlice";
-const Store = configureStore({
-      reducer:{
-            itemss:ItemReducer
-      }
-})
-
-export default Store
